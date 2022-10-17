9096eb7c174310dba6fd2233c6885c79db5ade8e
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-17 23:26:38 +0100

    update

--- get_print_func.c
+++ get_print_func.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * get_print_func - selects the correct function to perform the operation.
  * @s: argument indentifier
  * @index: index for argument indentifier
  * Return: pointer to a function.
- */
+*/
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
 	print_t pr[] = {
